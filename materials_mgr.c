7dcd19df4537a32c2725e7da6322ccb7138e4c90
delight <1304584803@qq.com>
2024-02-20 10:37:57 +0800

    无

--- materials_mgr.c
+++ materials_mgr.c
@@ -13,12 +13,12 @@ void lend_materials(void)
 {
     materials m1;
 
-    printf("请输入操作人工号：\n");
-    scanf("%s", m1.uid);
     printf("请输入物资编号：\n");
     scanf("%s", m1.id);
     printf("请输入借出数量：\n");
     scanf("%ld", &m1.num);
+    printf("请输入备注：\n");
+    scanf("%s", m1.bei);
 
     push_back2(ml, &m1);
 
@@ -49,7 +49,7 @@ void return_materials(void)
 
     if(m1 == NULL) 
     {
-        printf("物资不存在， 归还失败");
+        printf("物资不存在， 归还失败\n");
     }
     else
     {
@@ -66,7 +66,7 @@ void return_materials(void)
         fseek(fp, pos * sizeof(materials), SEEK_SET);
         fwrite(m1, sizeof(materials), 1, fp);
         fclose(fp);
-        printf("物资归还成功");
+        printf("物资归还成功\n");
     }
 }
 
@@ -80,10 +80,6 @@ void get_time(void)
 
     time(&timep); 
     p = localtime(&timep);  //取得当地时间 
-
-    //strcat(str, p->tm_year);
-    
-    printf("%d/%d/%d %d:%d:%d\n", (1900 + p->tm_year), 1 + p->tm_mon, p->tm_mday, p->tm_hour, p->tm_min, p->tm_sec);
 }
     
 
@@ -109,3 +105,12 @@ void init_ml(void)
 
     fclose(fp);
 }
+
+
+// 查看借出和归还物资记录
+/* void show_msg()
+{
+    printf("*************");
+    printf("借出记录\n");
+    printf("物资编号：%s");
+} */
